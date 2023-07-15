45247c38f586c80fea8bbfea120ae9625f1a3c82
Justin Swanson <justin.c.swanson@gmail.com>
2023-07-15 16:35:12 -0500

    PluginCollisionTest

---
+++ Spriggit.Tests/PluginCollisionTests.cs
@@ -0,0 +1,67 @@
+﻿using System.IO.Abstractions;
+using Autofac;
+using FluentAssertions;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Testing.AutoData;
+using Noggog;
+using Spriggit.Core;
+using Spriggit.Engine;
+using Xunit;
+
+namespace Spriggit.Tests;
+
+public class PluginCollisionTests
+{
+    [Theory, MutagenAutoData]
+    public async Task LoquiCollision(
+        IFileSystem fileSystem,
+        DirectoryPath existingFolder,
+        DirectoryPath existingOutputDir,
+        TestModule testModule)
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterModule(testModule);
+        var cont = builder.Build();
+        var c = cont.Resolve<EntryPointCache>();
+        var jsonSrc = new SpriggitSource()
+        {
+            Version = "0.0.0.1-dev",
+            PackageName = "Spriggit.Json.Skyrim"
+        };
+        var yamlSrc = new SpriggitSource()
+        {
+            Version = "0.0.0.1-dev",
+            PackageName = "Spriggit.Yaml.Skyrim"
+        };
+        var jsonEntry = await c.GetFor(new SpriggitMeta(
+                jsonSrc,
+                GameRelease.SkyrimSE),
+            CancellationToken.None);
+        jsonEntry.Should().NotBeNull();
+        var yamlEntry = await c.GetFor(new SpriggitMeta(
+                yamlSrc,
+                GameRelease.SkyrimSE),
+            CancellationToken.None);
+        yamlEntry.Should().NotBeNull();
+
+        var metaContent = @"SpriggitSource:
+  PackageName: Spriggit.Yaml.Skyrim
+  Version: 0.0.0.1-dev
+ModKey: ModKey.esp
+GameRelease: SkyrimSE";
+
+        var modFolder = Path.Combine(existingFolder, "ModKey.esp");
+        fileSystem.Directory.CreateDirectory(modFolder);
+        var modFile = Path.Combine(modFolder, "RecordData.yaml");
+        await fileSystem.File.WriteAllTextAsync(modFile, metaContent);
+
+        await yamlEntry!.EntryPoint.Deserialize(
+            modFolder,
+            workDropoff: null,
+            fileSystem: fileSystem,
+            streamCreator: null,
+            cancel: CancellationToken.None);
+    }
+}
---
+++ Spriggit.Tests/TestContainer.cs
@@ -0,0 +1,44 @@
+﻿using System.IO.Abstractions;
+using Autofac;
+using Mutagen.Bethesda.Serialization.Streams;
+using Noggog.Autofac;
+using Noggog.WorkEngine;
+using Serilog;
+using Serilog.Core;
+using Spriggit.Engine;
+
+namespace Spriggit.Tests;
+
+public class TestModule : Autofac.Module
+{
+    private readonly IFileSystem _fileSystem;
+
+    public TestModule(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+    
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyTypes(typeof(SpriggitEngine).Assembly)
+            .InNamespacesOf(
+                typeof(SpriggitEngine))
+            .Except<EntryPointWrapper>()
+            .AsImplementedInterfaces()
+            .AsSelf()
+            .SingleInstance();
+
+        builder.RegisterType<NoPreferenceStreamCreator>().AsImplementedInterfaces();
+        builder.RegisterType<NoPreferenceWorkDropoff>().AsImplementedInterfaces();
+        
+        builder.RegisterType<FileSystem>().As<IFileSystem>()
+            .SingleInstance();
+
+        builder.RegisterInstance(Logger.None).As<ILogger>();
+    }
+}
+
+public class TestContainer
+{
+    
+}
