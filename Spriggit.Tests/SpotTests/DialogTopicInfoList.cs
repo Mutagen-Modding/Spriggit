69e3946667d0b5da481d6bd9f47f1eebd0d6a081
Justin Swanson <justin.c.swanson@gmail.com>
2024-03-30 06:30:24 -0500

    Unit test for dialog topic infos

--- Spriggit.Tests/SpotTests/DialogTopicInfoList.cs
+++ Spriggit.Tests/SpotTests/DialogTopicInfoList.cs
@@ -1,4 +1,5 @@
 using System.IO.Abstractions;
+using DynamicData;
 using FluentAssertions;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins;
@@ -10,7 +11,7 @@ using Xunit;
 
 namespace Spriggit.Tests.SpotTests;
 
-public class DialogTests : SpotTestBase
+public class DialogTopicInfoList : SpotTestBase
 {
     [Theory, MutagenModAutoData(GameRelease.Starfield)]
     public async Task DialogReponses(
@@ -25,19 +26,31 @@ public class DialogTests : SpotTestBase
         var topic = quest.DialogTopics.AddReturn(new DialogTopic(mod));
         var resps = topic.Responses.AddReturn(new DialogResponses(mod));
         var resp = resps.Responses.AddReturn(new DialogResponse());
+        var resps2 = topic.Responses.AddReturn(new DialogResponses(mod));
+        var resp2 = resps2.Responses.AddReturn(new DialogResponse());
+        topic.TopicInfoList ??= new();
+        topic.TopicInfoList.Add(resps);
         resp.Edits = "Edits";
+        resp2.Edits = "Edits2";
 
         var reimport = await PassThrough(fileSystem, mod, dataFolder, spriggitFolder, otherModKey, entryPoint);
 
-        reimport.EnumerateMajorRecords().Should().HaveCount(3);
+        reimport.EnumerateMajorRecords().Should().HaveCount(4);
         reimport.Quests.Count.Should().Be(1);
         var reimportQuest = reimport.Quests.First();
         reimportQuest.DialogTopics.Count.Should().Be(1);
         var reimportTopic = reimportQuest.DialogTopics.First();
-        reimportTopic.Responses.Count.Should().Be(1);
+        reimportTopic.Responses.Count.Should().Be(2);
         var reimportResps = reimportTopic.Responses.First();
         reimportResps.Responses.Count.Should().Be(1);
         var reimportResp = reimportResps.Responses.First();
         reimportResp.Edits.Should().Be("Edits");
+        var reimportResps2 = reimportTopic.Responses.Last();
+        reimportResps.Responses.Count.Should().Be(1);
+        var reimportResp2 = reimportResps2.Responses.First();
+        reimportResp2.Edits.Should().Be("Edits2");
+        reimportTopic.TopicInfoList.Should().NotBeNull();
+        reimportTopic.TopicInfoList.Should().HaveCount(1);
+        reimportTopic.TopicInfoList!.First().FormKey.ID.Should().Be(resps.FormKey.ID);
     }
 }
