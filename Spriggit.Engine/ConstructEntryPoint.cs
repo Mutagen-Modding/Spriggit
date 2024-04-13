ed6dd6e83e7637b3c6db992fc3774e2271caead9
Justin Swanson <justin.c.swanson@gmail.com>
2024-04-13 15:18:18 -0500

    PreparePluginFolder service

--- Spriggit.Engine/ConstructEntryPoint.cs
+++ Spriggit.Engine/ConstructEntryPoint.cs
@@ -10,27 +10,23 @@ public class ConstructEntryPoint
 {
     private readonly IFileSystem _fileSystem;
     private readonly ILogger _logger;
-    private readonly NugetDownloader _nugetDownloader;
-    private readonly PluginPublisher _pluginPublisher;
+    private readonly PreparePluginFolder _preparePluginFolder;
     private readonly TargetFrameworkDirLocator _frameworkDirLocator;
     private readonly DebugState _debugState;
     private readonly ConstructAssemblyLoadedEntryPoint _constructAssemblyLoadedEntryPoint;
     private readonly GetFrameworkType _getFrameworkType;
 
     public ConstructEntryPoint(
-        IFileSystem fileSystem,
         ILogger logger,
-        NugetDownloader nugetDownloader,
-        PluginPublisher pluginPublisher,
+        PreparePluginFolder preparePluginFolder,
         TargetFrameworkDirLocator frameworkDirLocator,
         DebugState debugState,
         ConstructAssemblyLoadedEntryPoint constructAssemblyLoadedEntryPoint,
         GetFrameworkType getFrameworkType)
     {
-        _fileSystem = fileSystem;
+        _fileSystem = IFileSystemExt.DefaultFilesystem;
         _logger = logger;
-        _nugetDownloader = nugetDownloader;
-        _pluginPublisher = pluginPublisher;
+        _preparePluginFolder = preparePluginFolder;
         _frameworkDirLocator = frameworkDirLocator;
         _debugState = debugState;
         _constructAssemblyLoadedEntryPoint = constructAssemblyLoadedEntryPoint;
@@ -49,7 +45,7 @@ public class ConstructEntryPoint
 
         if (!rootDir.CheckExists())
         {
-            await PreparePluginFolder(ident, cancellationToken, rootDir);
+            await _preparePluginFolder.Prepare(ident, cancellationToken, rootDir);
         }
 
         var packageDir = Path.Combine(rootDir, $"{ident}");
@@ -59,6 +55,7 @@ public class ConstructEntryPoint
 
         var targetFrameworkDir = _fileSystem.Directory
             .EnumerateDirectories(libDir)
+            .Select(x => x)
             .OrderByDescending(x => x, new FrameworkLocatorComparer(_getFrameworkType, null))
             .FirstOrDefault();
 
@@ -78,59 +75,5 @@ public class ConstructEntryPoint
         if (frameworkDir == null) return null;
 
         return _constructAssemblyLoadedEntryPoint.GetEntryPoint(frameworkDir.Value, ident);
-    }
-
-    private async Task PreparePluginFolder(PackageIdentity ident, CancellationToken cancellationToken, DirectoryPath targetDir)
-    {
-        try
-        {
-            using var rootDir = TempFolder.Factory();
-            try
-            {
-                _logger.Information("Restoring for {Identifier} at {Path}", ident, rootDir.Dir);
-                await _nugetDownloader.RestoreFor(ident, rootDir.Dir, cancellationToken);
-            }
-            catch (InvalidOperationException ex)
-            {
-                _logger.Information("Issue restoring for {Ident} in {RootDir}",ident, rootDir.Dir);
-                cancellationToken.ThrowIfCancellationRequested();
-            }
-
-            var packageDir = Path.Combine(rootDir.Dir, $"{ident}");
-            var libDir = Path.Combine(packageDir, "lib");
-
-            _logger.Information("Finding target framework for {Dir}", libDir);
-
-            var targetFrameworkDir = _fileSystem.Directory
-                .EnumerateDirectories(libDir)
-                .OrderByDescending(x => x, new FrameworkLocatorComparer(_getFrameworkType, null))
-                .FirstOrDefault();
-
-            if (targetFrameworkDir == null)
-            {
-                throw new DirectoryNotFoundException($"Could not find target framework directory within {libDir}");
-            }
-
-            var targetFramework = Path.GetFileName(targetFrameworkDir);
-
-            if (targetFramework == null)
-            {
-                throw new DirectoryNotFoundException($"Could not find target framework directory within {libDir}");
-            }
-
-            var frameworkDir = _frameworkDirLocator.GetTargetFrameworkDir(packageDir, targetFramework);
-            if (frameworkDir == null) return;
-
-            _logger.Information("Publishing {Root} into framework directory {Path} for {Identifier}", rootDir.Dir, frameworkDir,
-                ident);
-            _pluginPublisher.Publish(rootDir.Dir, ident.ToString(), frameworkDir.Value, targetFramework);
-            Directory.CreateDirectory(Path.GetDirectoryName(targetDir)!);
-            Directory.Move(rootDir.Dir, targetDir);
-        }
-        catch (Exception)
-        {
-            targetDir.DeleteEntireFolder();
-            throw;
-        }
     }
 }
---
+++ Spriggit.Engine/PreparePluginFolder.cs
@@ -0,0 +1,86 @@
+﻿using System.IO.Abstractions;
+using Noggog;
+using Noggog.IO;
+using NuGet.Packaging.Core;
+using Serilog;
+
+namespace Spriggit.Engine;
+
+public class PreparePluginFolder
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly ILogger _logger;
+    private readonly NugetDownloader _nugetDownloader;
+    private readonly TargetFrameworkDirLocator _frameworkDirLocator;
+    private readonly GetFrameworkType _getFrameworkType;
+    private readonly PluginPublisher _pluginPublisher;
+
+    public PreparePluginFolder(
+        ILogger logger,
+        NugetDownloader nugetDownloader, 
+        TargetFrameworkDirLocator frameworkDirLocator,
+        GetFrameworkType getFrameworkType,
+        PluginPublisher pluginPublisher)
+    {
+        _logger = logger;
+        _fileSystem = IFileSystemExt.DefaultFilesystem;
+        _nugetDownloader = nugetDownloader;
+        _frameworkDirLocator = frameworkDirLocator;
+        _getFrameworkType = getFrameworkType;
+        _pluginPublisher = pluginPublisher;
+    }
+    
+    public async Task Prepare(PackageIdentity ident, CancellationToken cancellationToken, DirectoryPath targetDir)
+    {
+        try
+        {
+            using var rootDir = TempFolder.Factory();
+            try
+            {
+                _logger.Information("Restoring for {Identifier} at {Path}", ident, rootDir.Dir);
+                await _nugetDownloader.RestoreFor(ident, rootDir.Dir, cancellationToken);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Information("Issue restoring for {Ident} in {RootDir}",ident, rootDir.Dir);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+
+            var packageDir = Path.Combine(rootDir.Dir, $"{ident}");
+            var libDir = Path.Combine(packageDir, "lib");
+
+            _logger.Information("Finding target framework for {Dir}", libDir);
+
+            var targetFrameworkDir = _fileSystem.Directory
+                .EnumerateDirectories(libDir)
+                .OrderByDescending(x => x, new FrameworkLocatorComparer(_getFrameworkType, null))
+                .FirstOrDefault();
+
+            if (targetFrameworkDir == null)
+            {
+                throw new DirectoryNotFoundException($"Could not find target framework directory within {libDir}");
+            }
+
+            var targetFramework = Path.GetFileName(targetFrameworkDir);
+
+            if (targetFramework == null)
+            {
+                throw new DirectoryNotFoundException($"Could not find target framework directory within {libDir}");
+            }
+
+            var frameworkDir = _frameworkDirLocator.GetTargetFrameworkDir(packageDir, targetFramework);
+            if (frameworkDir == null) return;
+
+            _logger.Information("Publishing {Root} into framework directory {Path} for {Identifier}", rootDir.Dir, frameworkDir,
+                ident);
+            _pluginPublisher.Publish(rootDir.Dir, ident.ToString(), frameworkDir.Value, targetFramework);
+            Directory.CreateDirectory(Path.GetDirectoryName(targetDir)!);
+            Directory.Move(rootDir.Dir, targetDir);
+        }
+        catch (Exception)
+        {
+            targetDir.DeleteEntireFolder();
+            throw;
+        }
+    }
+}
