4ae8808c96c1af2647f49f73e1084a38914080a8
Justin Swanson <justin.c.swanson@gmail.com>
2024-05-06 14:23:12 -0500

    FormIDCollisionFixer work

---
+++ Spriggit.Engine/Collision/FormIDCollisionFixer.cs
@@ -0,0 +1,91 @@
+﻿using System.IO.Abstractions;
+using LibGit2Sharp;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+using Noggog;
+using Noggog.IO;
+using Spriggit.Core;
+
+namespace Spriggit.Engine.Collision;
+
+public class FormIDCollisionFixer
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly FormIDCollisionDetector _detector;
+
+    public FormIDCollisionFixer(
+        IFileSystem fileSystem,
+        FormIDCollisionDetector detector)
+    {
+        _fileSystem = fileSystem;
+        _detector = detector;
+    }
+
+    public async Task DetectAndFix<TMod, TModGetter>(
+        ModKey modKey,
+        GameRelease release,
+        IEntryPoint entryPoint,
+        DirectoryPath gitRootPath,
+        DirectoryPath spriggitModPath)
+        where TMod : class, IContextMod<TMod, TModGetter>, TModGetter
+        where TModGetter : class, IContextGetterMod<TMod, TModGetter>
+    {
+        using var tmp = TempFolder.Factory(fileSystem: _fileSystem);
+
+        var mergedModPath = Path.Combine(tmp.Dir, modKey.FileName);
+
+        await entryPoint.Deserialize(
+            spriggitModPath,
+            mergedModPath,
+            workDropoff: null,
+            fileSystem: _fileSystem,
+            streamCreator: null,
+            cancel: CancellationToken.None);
+
+        var mergedMod = ModInstantiator<TMod>.Importer(mergedModPath, release, fileSystem: _fileSystem);
+        
+        var collisions = _detector.LocateCollisions(mergedMod);
+        if (collisions.Count == 0) return;
+
+        var repo = new LibGit2Sharp.Repository(gitRootPath);
+        if (repo == null)
+        {
+            throw new Exception("No git repository detected");
+        }
+
+        if (!repo.Head.Commits.Any())
+        {
+            throw new Exception("Git repository had no commits at HEAD");
+        }
+        
+        var parents = repo.Head.Commits.First().Parents
+            .ToArray();
+
+        if (parents.Length != 2)
+        {
+            throw new Exception("Git did not have a merge commit at HEAD");
+        }
+
+        var branch = repo.CreateBranch("Spriggit-Merge-Fix", parents[0]);
+        Commands.Checkout(repo, branch);
+
+        //
+        // var cache = mergedMod.ToMutableLinkCache<TMod, TModGetter>();
+        //
+        // var remapping = new Dictionary<FormKey, FormKey>();
+        //
+        // foreach (var collList in collisions)
+        // {
+        //     if (collList.Value.Count == 0) continue;
+        //
+        //     foreach (var other in collList.Value.Skip(1))
+        //     {
+        //         var context = cache.ResolveContext(other.FormKey, other.GetType());
+        //         mod.Remove(other.FormKey, other.GetType());
+        //         var newRec = context.DuplicateIntoAsNewRecord(mod);
+        //         remapping[]
+        //     }
+        // }
+    }
+}
---
+++ Spriggit.Tests/Collision/FormIDCollisionFixerTests.cs
@@ -0,0 +1,170 @@
+﻿using System.IO.Abstractions;
+using FluentAssertions;
+using LibGit2Sharp;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Binary.Parameters;
+using Mutagen.Bethesda.Starfield;
+using Mutagen.Bethesda.Testing.AutoData;
+using Noggog;
+using Noggog.IO;
+using Noggog.Testing.AutoFixture;
+using Spriggit.Core;
+using Spriggit.Engine.Collision;
+using Spriggit.Yaml.Starfield;
+using Xunit;
+
+namespace Spriggit.Tests.Collision;
+
+public class FormIDCollisionFixerTests
+{
+    [Theory, MutagenModAutoData(GameRelease.Starfield)]
+    public async Task NothingToFix(
+        IFileSystem fileSystem,
+        StarfieldMod mod,
+        Npc n1,
+        ModKey modKey,
+        EntryPoint entryPoint,
+        DirectoryPath modFolder,
+        DirectoryPath modFolder2,
+        DirectoryPath gitRootPath,
+        DirectoryPath spriggitModPath,
+        FormIDCollisionFixer sut)
+    {
+        var modPath = Path.Combine(modFolder, mod.ModKey.FileName);
+        fileSystem.Directory.CreateDirectory(modFolder);
+        mod.WriteToBinary(modPath, fileSystem: fileSystem);
+        await entryPoint.Serialize(
+            modPath, spriggitModPath, GameRelease.Starfield,
+            null, fileSystem, null,
+            new SpriggitSource()
+            {
+                PackageName = "Spriggit.Yaml.Starfield",
+                Version = "Test"
+            },
+            CancellationToken.None);
+
+        await sut.DetectAndFix<IStarfieldMod, IStarfieldModGetter>(
+            modKey, GameRelease.Starfield,
+            entryPoint,
+            gitRootPath: gitRootPath,
+            spriggitModPath: spriggitModPath);
+
+        var modPath2 = Path.Combine(modFolder2, mod.ModKey.FileName);
+        fileSystem.Directory.CreateDirectory(modFolder2);
+        await entryPoint.Deserialize(
+            inputPath: spriggitModPath,
+            outputPath: modPath2,
+            fileSystem: fileSystem,
+            workDropoff: null,
+            streamCreator: null,
+            cancel: CancellationToken.None);
+
+        var reimport = StarfieldMod.CreateFromBinary(modPath2, StarfieldRelease.Starfield, fileSystem: fileSystem);
+        reimport.EnumerateMajorRecords().Should().HaveCount(1);
+        reimport.Npcs.First().FormKey.Should().Be(n1.FormKey);
+    }
+
+    [Theory, MutagenModAutoData(GameRelease.Starfield, TargetFileSystem.Real)]
+    public async Task SomethingToFix(
+        StarfieldMod mod,
+        Armor armor,
+        EntryPoint entryPoint,
+        FormIDCollisionFixer sut)
+    {
+        var spriggitSource = new SpriggitSource()
+        {
+            PackageName = "Spriggit.Yaml.Starfield",
+            Version = "Test"
+        };
+        using var tmp = TempFolder.FactoryByAddedPath("FormIdCollisionFixer");
+        tmp.Dir.DeleteEntireFolder();
+        
+        var modFolder = Path.Combine(tmp.Dir, "ModFolder");
+        Directory.CreateDirectory(modFolder);
+        var modPath = Path.Combine(modFolder, mod.ModKey.FileName);
+        var repoPath = Path.Combine(tmp.Dir, "Repo");
+        Repository.Init(repoPath);
+        var signature = new Signature("me", "someone@gmail.com", DateTimeOffset.Now);
+        using var repo = new Repository(repoPath);
+        var spriggitModPath = Path.Combine(repoPath, "Spriggit");
+        
+        // Make initial content
+        File.WriteAllText(Path.Combine(tmp.Dir, "Readme.md"), "Readme");
+        mod.WriteToBinary(modPath);
+        await entryPoint.Serialize(
+            modPath, spriggitModPath, GameRelease.Starfield,
+            null, null, null,
+            spriggitSource,
+            CancellationToken.None);
+
+        Commands.Stage(repo, "*");
+        repo.Commit("Initial", signature, signature, new CommitOptions());
+        
+        // Make branches
+        var lhs = repo.CreateBranch("lhs");
+        var rhs = repo.CreateBranch("rhs");
+
+        var formKeyToCollide = mod.GetNextFormKey();
+
+        // Setup lhs
+        {
+            Commands.Checkout(repo, lhs);
+            var weap = new Weapon(formKeyToCollide, StarfieldRelease.Starfield);
+            mod.Weapons.Add(weap);
+            mod.WriteToBinary(modPath);
+            
+            await entryPoint.Serialize(
+                modPath, spriggitModPath, GameRelease.Starfield,
+                null, null, null,
+                spriggitSource,
+                CancellationToken.None);
+            
+            Commands.Stage(repo, "*");
+            repo.Commit("Weapon", signature, signature, new CommitOptions());
+            mod.Weapons.Clear();
+        }
+
+        // Setup rhs
+        {
+            Commands.Checkout(repo, rhs);
+            var npc = new Npc(formKeyToCollide, StarfieldRelease.Starfield);
+            mod.Npcs.Add(npc);
+            mod.WriteToBinary(modPath);
+            
+            await entryPoint.Serialize(
+                modPath, spriggitModPath, GameRelease.Starfield,
+                null, null, null,
+                spriggitSource,
+                CancellationToken.None);
+            
+            Commands.Stage(repo, "*");
+            repo.Commit("Npc", signature, signature, new CommitOptions());
+            mod.Npcs.Clear();
+        }
+        
+        // Merge
+        repo.Merge(lhs, signature, new MergeOptions());
+
+        await sut.DetectAndFix<IStarfieldMod, IStarfieldModGetter>(
+            mod.ModKey, GameRelease.Starfield,
+            entryPoint,
+            gitRootPath: repoPath,
+            spriggitModPath: spriggitModPath);
+        //
+        // var modPath2 = Path.Combine(modFolder2, mod.ModKey.FileName);
+        // Directory.CreateDirectory(modFolder2);
+        // await entryPoint.Deserialize(
+        //     inputPath: spriggitModPath,
+        //     outputPath: modPath2,
+        //     fileSystem: null,
+        //     workDropoff: null,
+        //     streamCreator: null,
+        //     cancel: CancellationToken.None);
+        //
+        // var reimport = StarfieldMod.CreateFromBinary(modPath2, StarfieldRelease.Starfield);
+        // reimport.EnumerateMajorRecords().Should().HaveCount(2);
+        // reimport.Npcs.First().FormKey.Should().Be(n1.FormKey);
+        // reimport.Weapons.First().FormKey.Should().Be(weap.FormKey);
+    }
+}
