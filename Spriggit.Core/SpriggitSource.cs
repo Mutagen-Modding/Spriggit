e50ed8aa3d5886be2abae01072fa3103e118ebd5
Justin Swanson <justin.c.swanson@gmail.com>
2024-06-08 21:41:43 -0500

    SpriggitSource equality

--- Spriggit.Core/SpriggitSource.cs
+++ Spriggit.Core/SpriggitSource.cs
@@ -18,4 +18,32 @@ public class SpriggitSource
             return $"{PackageName}.{Version}";
         }
     }
+    
+    protected bool Equals(SpriggitSource other)
+    {
+        return PackageName == other.PackageName && Version == other.Version;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((SpriggitSource)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(PackageName, Version);
+    }
+
+    public static bool operator ==(SpriggitSource? left, SpriggitSource? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(SpriggitSource? left, SpriggitSource? right)
+    {
+        return !Equals(left, right);
+    }
 }
