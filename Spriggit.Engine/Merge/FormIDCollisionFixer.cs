11bc3c4e3cd40e3e5028e53c9a0fdc37ae9435e2
Justin Swanson <justin.c.swanson@gmail.com>
2024-06-16 11:46:27 -0500

    More logging added to collision fix command

--- Spriggit.Engine/Merge/FormIDCollisionFixer.cs
+++ Spriggit.Engine/Merge/FormIDCollisionFixer.cs
@@ -6,6 +6,7 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins.Records;
 using Noggog;
 using Noggog.IO;
+using Serilog;
 using Spriggit.Core;
 using Spriggit.Engine.Services.Singletons;
 
@@ -13,6 +14,7 @@ namespace Spriggit.Engine.Merge;
 
 public class FormIDCollisionFixer
 {
+    private readonly ILogger _logger;
     private readonly IFileSystem _fileSystem;
     private readonly FormIDReassigner _reassigner;
     private readonly IEntryPointCache _entryPointCache;
@@ -21,6 +23,7 @@ public class FormIDCollisionFixer
     private readonly FormIDCollisionDetector _detector;
 
     public FormIDCollisionFixer(
+        ILogger logger,
         IFileSystem fileSystem,
         FormIDReassigner reassigner,
         IEntryPointCache entryPointCache,
@@ -28,6 +31,7 @@ public class FormIDCollisionFixer
         GitFolderLocator gitFolderLocator,
         FormIDCollisionDetector detector)
     {
+        _logger = logger;
         _fileSystem = fileSystem;
         _reassigner = reassigner;
         _entryPointCache = entryPointCache;
@@ -75,6 +79,7 @@ public class FormIDCollisionFixer
         FilePath origMergedModPath = Path.Combine(tmp.Dir, "MergedOrig", meta.ModKey.FileName);
         origMergedModPath.Directory?.Create(_fileSystem);
 
+        _logger.Information("Deserializing mod to {Path}", origMergedModPath);
         await entryPoint.Deserialize(
             spriggitModPath,
             origMergedModPath,
@@ -85,8 +90,13 @@ public class FormIDCollisionFixer
 
         var origMergedMod = ModInstantiator<TMod>.Importer(origMergedModPath.Path, meta.Release, fileSystem: _fileSystem);
         
+        _logger.Information("Locating collisions");
         var collisions = _detector.LocateCollisions(origMergedMod);
-        if (collisions.Count == 0) return;
+        if (collisions.Count == 0)
+        {
+            _logger.Information("No collisions found. Exiting");
+            return;
+        }
 
         foreach (var coll in collisions)
         {
@@ -100,7 +110,15 @@ public class FormIDCollisionFixer
             .Select(x => x.ToFormLinkInformation())
             .ToArray();
 
+        _logger.Information("Reassigning:");
+        foreach (var id in toReassign)
+        {
+            _logger.Information($"  {id}");
+        }
+
         var gitRootPath = _gitFolderLocator.Get(spriggitModPath);
+        
+        _logger.Information("Creating repo at {Path}", gitRootPath);
 
         var repo = new LibGit2Sharp.Repository(gitRootPath);
         if (repo == null)
@@ -114,21 +132,31 @@ public class FormIDCollisionFixer
         }
 
         var fixSignature = repo.Head.Tip.Author;
-        
-        var parents = repo.Head.Commits.First().Parents
+        var commit = repo.Head.Commits.First();
+        var parents = commit.Parents
             .ToArray();
+        
+        _logger.Information("Considering commit {Sha}", commit.Sha);
 
         if (parents.Length != 2)
         {
             throw new Exception("Git did not have a merge commit at HEAD");
         }
         
+        _logger.Information("Parent commits:");
+        _logger.Information($"  {parents[0].Sha}");
+        _logger.Information($"  {parents[1].Sha}");
+        
         var origBranch = repo.Head;
         var branchName = $"Spriggit-Merge-Fix-{origBranch.Tip.Sha.Substring(0, 6)}";
+        
+        _logger.Information("Creating branch {Branch} at {Sha}", branchName, parents[0].Sha);
+        
         repo.Branches.Remove(branchName);
         var branch = repo.CreateBranch(branchName, parents[0]);
         Commands.Checkout(repo, branch);
 
+        _logger.Information("Deserializing mod to {Path}", origMergedModPath);        
         await entryPoint.Deserialize(
             spriggitModPath,
             origMergedModPath,
@@ -139,13 +167,16 @@ public class FormIDCollisionFixer
 
         var branchMod = ModInstantiator<TMod>.Importer(origMergedModPath.Path, meta.Release, fileSystem: _fileSystem);
 
+        _logger.Information("Executing reassignment");
         _reassigner.Reassign<TMod, TModGetter>(
             branchMod, 
             () => origMergedMod.GetNextFormKey(),
             toReassign);
         
+        _logger.Information("Writing reassigned mod to {Path}", origMergedModPath.Path);
         branchMod.WriteToBinary(origMergedModPath.Path);
         
+        _logger.Information("Serializing mod to {Path}", spriggitModPath);  
         await entryPoint.Serialize(
             origMergedModPath.Path,
             spriggitModPath,
@@ -156,18 +187,24 @@ public class FormIDCollisionFixer
             meta: meta.Source,
             cancel: CancellationToken.None);
 
+        _logger.Information("Committing changes");
         Commands.Checkout(repo, branch);
         Commands.Stage(repo, Path.Combine(spriggitModPath, "*"));
         repo.Commit("FormID Collision Fix", fixSignature, fixSignature, new CommitOptions());
 
+        _logger.Information("Checking out original branch {Branch}", origBranch.CanonicalName);
         Commands.Checkout(repo, origBranch);
+        _logger.Information("Merging fix into {Branch}", origBranch.CanonicalName);
         repo.Merge(branch, fixSignature);
         
+        _logger.Information("Deleting fix branch {Branch}", branch.CanonicalName);
         repo.Branches.Remove(branch);
         
         FilePath newMergedModPath = Path.Combine(tmp.Dir, "MergedNew", meta.ModKey.FileName);
+        _logger.Information("Creating result temp folder {Path}", newMergedModPath.Directory);
         newMergedModPath.Directory?.Create(_fileSystem);
 
+        _logger.Information("Deserializing mod to {Path}", newMergedModPath);  
         await entryPoint.Deserialize(
             spriggitModPath,
             newMergedModPath,
@@ -176,6 +213,7 @@ public class FormIDCollisionFixer
             streamCreator: null,
             cancel: CancellationToken.None);
 
+        _logger.Information("Checking for collisions in fixed result");
         var newMergedMod = ModInstantiator<TMod>.Importer(newMergedModPath.Path, meta.Release, fileSystem: _fileSystem);
 
         var newCollisions = _detector.LocateCollisions(newMergedMod);
@@ -183,5 +221,7 @@ public class FormIDCollisionFixer
         {
             throw new Exception($"Fix still had collided FormIDs.  Leaving in a bad state");
         }
+        
+        _logger.Information("Collision fix complete");
     }
 }
