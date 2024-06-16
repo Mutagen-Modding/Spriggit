72448e6ba62485015b5e069d8554be50caf1c4cd
Justin Swanson <justin.c.swanson@gmail.com>
2024-06-16 10:36:30 -0500

    Try/Catch/Print CLI commands

--- Spriggit.CLI/Program.cs
+++ Spriggit.CLI/Program.cs
@@ -4,16 +4,25 @@ using Spriggit.CLI;
 using Spriggit.CLI.Commands;
 using Spriggit.Engine.Services.Singletons;
 
-CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+try
+{
+    CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-var vers = new CurrentVersionsProvider();
-Console.WriteLine($"Spriggit version {vers.SpriggitVersion}");
+    var vers = new CurrentVersionsProvider();
+    Console.WriteLine($"Spriggit version {vers.SpriggitVersion}");
 
-return await Parser.Default.ParseArguments(args, typeof(DeserializeCommand), typeof(SerializeCommand), typeof(FormIDCollisionCommand))
-    .MapResult(
-        async (DeserializeCommand deserialize) => await EngineRunner.Run(deserialize, null),
-        async (SerializeCommand serialize) => await EngineRunner.Run(serialize, null),
-        async (FormIDCollisionCommand formIdCollision) => await FormIDCollisionRunner.Run(formIdCollision),
-        async (MergeVersionSyncerCommand versionSyncer) => await MergeVersionSyncerRunner.Run(versionSyncer),
-        async _ => -1);
+    return await Parser.Default.ParseArguments(args, typeof(DeserializeCommand), typeof(SerializeCommand), typeof(FormIDCollisionCommand))
+        .MapResult(
+            async (DeserializeCommand deserialize) => await EngineRunner.Run(deserialize, null),
+            async (SerializeCommand serialize) => await EngineRunner.Run(serialize, null),
+            async (FormIDCollisionCommand formIdCollision) => await FormIDCollisionRunner.Run(formIdCollision),
+            async (MergeVersionSyncerCommand versionSyncer) => await MergeVersionSyncerRunner.Run(versionSyncer),
+            async _ => -1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error:");
+    Console.WriteLine(ex);
+    return -1;
+}
