a72b3ca87eb24d18ab7c70a3c8af24a28c156aa0
Justin Swanson <justin.c.swanson@gmail.com>
2025-04-28 00:57:22 -0500

    AddNonOpinionatedWriteOptions reuse

--- Translation Packages/Spriggit.Json.Fallout4/EntryPoint.cs
+++ Translation Packages/Spriggit.Json.Fallout4/EntryPoint.cs
@@ -8,6 +8,7 @@ using Noggog;
 using Noggog.IO;
 using Noggog.WorkEngine;
 using Spriggit.Core;
+using Spriggit.TranslationPackages;
 
 namespace Spriggit.Json.Fallout4;
 
@@ -65,14 +66,7 @@ public class EntryPoint : IEntryPoint
             .WithLoadOrderFromHeaderMasters()
             .WithDataFolder(dataPath)
             .WithFileSystem(fileSystem)
-            .WithRecordCount(RecordCountOption.Iterate)
-            .WithModKeySync(ModKeyOption.CorrectToPath)
-            .WithMastersListContent(MastersListContentOption.NoCheck)
-            .WithMastersListOrdering(MastersListOrderingOption.NoCheck)
-            .NoFormIDUniquenessCheck()
-            .NoFormIDCompactnessCheck()
-            .NoCheckIfLowerRangeDisallowed()
-            .NoNullFormIDStandardization()
+            .AddNonOpinionatedWriteOptions()
             .WriteAsync();
     }
 
