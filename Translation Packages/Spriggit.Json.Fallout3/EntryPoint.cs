7e962f1ab9c13a437e61c843efb1675cf5ced971
Justin Swanson <justin.c.swanson@gmail.com>
2026-07-01 19:08:18 -0500

    Fo3 support and nuget bump

---
+++ Translation Packages/Spriggit.Json.Fallout3/EntryPoint.cs
@@ -0,0 +1,75 @@
+using System.IO.Abstractions;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Binary.Parameters;
+using Mutagen.Bethesda.Serialization.Newtonsoft;
+using Mutagen.Bethesda.Serialization.Utility;
+using Mutagen.Bethesda.Fallout3;
+using Noggog;
+using Noggog.IO;
+using Noggog.WorkEngine;
+using Spriggit.Core;
+using Spriggit.TranslationPackages;
+
+namespace Spriggit.Json.Fallout3;
+
+public class EntryPoint : IEntryPoint
+{
+    public async Task Serialize(
+        ModPath modPath,
+        DirectoryPath outputDir,
+        DirectoryPath? dataPath,
+        KnownMaster[] knownMasters,
+        GameRelease release,
+        IWorkDropoff? workDropoff,
+        IFileSystem? fileSystem,
+        ICreateStream? streamCreator,
+        SpriggitSource meta,
+        bool throwOnUnknown,
+        CancellationToken cancel)
+    {
+        fileSystem = fileSystem.GetOrDefault();
+        using var modGetter = Fallout3Mod
+            .Create(release.ToFallout3Release())
+            .FromPath(modPath)
+            .WithDataFolder(dataPath)
+            .WithFileSystem(fileSystem)
+            .ThrowIfUnknownSubrecord(shouldThrow: throwOnUnknown)
+            .Construct();
+        await MutagenJsonConverter.Instance.Serialize(
+            modGetter,
+            outputDir,
+            workDropoff: workDropoff,
+            fileSystem: fileSystem,
+            streamCreator: streamCreator,
+            extraMeta: meta,
+            cancel: cancel);
+    }
+
+    public async Task Deserialize(
+        string inputPath,
+        string outputPath,
+        DirectoryPath? dataPath,
+        KnownMaster[] knownMasters,
+        IWorkDropoff? workDropoff,
+        IFileSystem? fileSystem,
+        ICreateStream? streamCreator,
+        CancellationToken cancel)
+    {
+        var mod = await MutagenJsonConverter.Instance.Deserialize(
+            inputPath,
+            workDropoff: workDropoff,
+            fileSystem: fileSystem,
+            streamCreator: streamCreator,
+            cancel: cancel);
+        await mod.BeginWrite
+            .ToPath(outputPath)
+            .WithLoadOrderFromHeaderMasters()
+            .WithDataFolder(dataPath)
+            .WithFileSystem(fileSystem)
+            .AddNonOpinionatedWriteOptions()
+            .WriteAsync();
+    }
+
+    private static readonly Mutagen.Bethesda.Serialization.Newtonsoft.NewtonsoftJsonSerializationReaderKernel ReaderKernel = new();
+}
